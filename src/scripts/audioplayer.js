df98e2be823e22481df06a614f3fe46adddb6946
Luca de Nitto <luca@dn-webdesign.de>
2020-06-19 15:03:20 +0200

    Grobe Struktur

--- src/scripts/audioplayer.js
+++ src/scripts/audioplayer.js
@@ -1,1 +1,35 @@
-// eslint-disable-next-line import/extensions
+class Playlist {
+  constructor(playlist) {
+    this.playlist = playlist;
+    this.tracks = playlist.querySelectorAll('.playlist__track');
+
+    this.init();
+  }
+
+  init() {
+    this.tracks.forEach((track, i) => {
+      track = new Howl({
+        src: [track.getAttribute('data-js-track-src')],
+      });
+
+      track.addEventListener('click', () => {
+        this.play(i);
+      });
+    });
+  }
+
+  play(index) {
+    //Alle anderen stoppen
+    this.tracks.forEach((track, id) => {
+      this.stop(id);
+    });
+    //Angeklickten abspielen
+    this.tracks[index].play();
+  }
+
+  stop(index) {
+    this.tracks[index].stop();
+  }
+}
+
+const playlist = new Playlist(document.querySelector('.playlist'));
